bcb5e63636dd839f384058854cd590beb815e9ac
隅田 勝将 <vortex722@gmail.com>
2018-05-29 19:04:05 +0900

    first commit

---
+++ src/main/java/sumi/service/GoogleSearchList.java
@@ -0,0 +1,152 @@
+package sumi.service;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class GoogleSearchList {
+	public static void getHtml() {
+		//String url = "http://ypage.tennis365.net/ypage/index_detail.html?ypage_id=4127";
+		String url = "https://www.google.co.jp/search?biw=1280&bih=590&ei=wOQMW840x6DxBfTYuLAG&q=site%3Ahttps%3A%2F%2Fproduct.rakuten.co.jp%2Fproduct%2F&oq=site%3Ahttps%3A%2F%2Fproduct.rakuten.co.jp%2Fproduct%2F&gs_l=psy-ab.12...0.0.0.240655.0.0.0.0.0.0.0.0..0.0....0...1c..64.psy-ab..0.0.0....0.WMlWE3A-Ln8";
+
+		try {
+			Document doc = Jsoup.connect(url).get();
+			System.out.println(doc);
+			Elements as = doc.select("a");
+
+			for(Element a : as) {
+				System.out.println(a.attr("src"));
+				//System.out.println(img);
+			}
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+	}
+
+	public static void getProductLink() {
+
+		String url = "https://search.rakuten.co.jp/search/mall/-/502368/";
+		int page = 10;
+		String filename = "productLink.txt";
+
+
+		File file = new File(filename);
+		try {
+			FileWriter filewriter = new FileWriter(file);
+
+			WebDriver driver = new PhantomJSDriver();
+			driver.get(url);
+
+			Boolean loop = true;
+			int counter = 0;
+			while(loop) {
+				counter++;
+				System.out.println("Page : " + counter);
+				//(new WebDriverWait(driver,10)).until( d -> d.getTitle().startsWith("ほりえもん"));
+				//new WebDriverWait(driver, 100);
+
+				List<WebElement> elements = driver.findElements(By.linkText("最安ショップを見る"));
+				for(WebElement e : elements) {
+					//System.out.println(e.getText());
+					System.out.println(e.getAttribute("href"));
+
+					filewriter.write(e.getAttribute("href") + "\r\n");
+				}
+
+				try {
+					WebElement nextbtn = driver.findElement(By.linkText("次のページ"));
+					nextbtn.click();
+				} catch (NoSuchElementException e) {
+					//要素がなくなった場合、ループを終了する
+			        loop = false;
+			    }
+
+				if(counter>10) {
+					loop=false;
+				}
+
+			}
+
+
+	        System.out.println("End:");
+	        filewriter.close();
+
+			driver.quit();
+
+		} catch (IOException e1) {
+			// TODO 自動生成された catch ブロック
+			e1.printStackTrace();
+		}
+	}
+
+	public static void getHtmlByPhontom() {
+
+		String url = "http://www.google.com";
+		int page = 10;
+		String filename = "output.txt";
+
+
+		File file = new File(filename);
+		try {
+			FileWriter filewriter = new FileWriter(file);
+
+			WebDriver driver = new PhantomJSDriver();
+			driver.get(url);
+
+			WebElement element = driver.findElement(By.name("q"));
+			element.sendKeys("site://https://product.rakuten.co.jp/product/");
+			element.submit();
+
+			Boolean loop = true;
+			int counter = 0;
+			while(loop) {
+				counter++;
+				System.out.println("Page : " + counter);
+				//(new WebDriverWait(driver,10)).until( d -> d.getTitle().startsWith("ほりえもん"));
+				new WebDriverWait(driver, 100);
+
+				List<WebElement> elements = driver.findElements(By.cssSelector("h3.r>a"));
+				for(WebElement e : elements) {
+					//System.out.println(e.getText());
+					//System.out.println(e.getAttribute("href"));
+
+					filewriter.write(e.getText() + "\t" + e.getAttribute("href") + "\r\n");
+				}
+
+				try {
+					WebElement nextbtn = driver.findElement(By.linkText("次へ"));
+					nextbtn.click();
+				} catch (NoSuchElementException e) {
+					//要素がなくなった場合、ループを終了する
+			        loop = false;
+			    }
+			}
+
+
+	        System.out.println("End:");
+	        filewriter.close();
+
+			driver.quit();
+
+		} catch (IOException e1) {
+			// TODO 自動生成された catch ブロック
+			e1.printStackTrace();
+		}
+	}
+
+}
---
+++ src/main/java/sumi/service/Main.java
@@ -0,0 +1,14 @@
+package sumi.service;
+
+public class Main {
+	  public static void main(String[] args) {
+	        try {
+	        	//GoogleSearchList.getHtml();
+	        	GoogleSearchList.getProductLink();
+	        	//GCSList();
+			} catch (Exception e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+	  }
+}
